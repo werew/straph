c2e5880aa94c382993e5ecb7de6cec51744e0782
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2017-02-02 13:59:13 +0100

    Adding a fail function for debug purpouses

--- src/straph.c
+++ src/straph.c
@@ -778,15 +778,31 @@ int launch_straph(straph s){
 
 
 
+
+/**
+ * Terminates the program printing an error message
+ * @param msg Error message
+ * @param line Number of the line
+ * @param func Name of the function
+ *
+ * @note Do not use this function direclty, rather use
+ *       the wrapping macro "fail(msg)
+ */
+void _fail(const char* msg, int line, const char* func){
+    fprintf(stderr, "Error at line %d (function %s):\n",line,func);
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+#define fail(x) _fail(x, __LINE__, __func__)
+
+
 void* test(node n){
     char buf[10];
-    size_t l = st_read(n,0,buf,6);
+    size_t l = st_read(n,0,buf,7);
     printf("length: %ld \"%s\"\n",l,buf); 
     st_write(n,0,"hello",6);
     return NULL;
 }
-
-
 
 int main(void){
     straph s = new_straph();
@@ -795,17 +811,19 @@ int main(void){
     int i;
     for (i =0; i<10; i++){
         ns[i] = new_node(test);
+        if (ns[i] == NULL) fail("new_node");
     } 
 
     for (i=0; i<9; i++){
-        set_buffer(ns[i], 0, LIN_BUF, 10); //XXX this should not be necessary
-        link_nodes(ns[i],0,ns[i+1],0, SEQ_MODE);
+        //XXX this should not be necessary
+        if (set_buffer(ns[i], 0, LIN_BUF, 10) != 0) fail("set_buffer");
+        if (link_nodes(ns[i],0,ns[i+1],0, SEQ_MODE) != 0) fail("link_nodes");
     }
     
-    add_start_node(s, ns[0]);
-    launch_straph(s);
-    join_straph(s);
-    straph_destroy(s);
+    if (add_start_node(s, ns[0]) != 0) fail("add_start_node");
+    if (launch_straph(s) != 0) fail("launch_straph");
+    if (join_straph(s) != 0) fail("join_straph");
+    if (straph_destroy(s) != 0) fail("straph_destroy");
 
     
     return 0;
