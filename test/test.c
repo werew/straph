a30a5ec249e7e75a0decb291a0489e5b7c75b712
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2017-08-01 17:12:51 +0200

    Use a very simple test

--- test/test.c
+++ test/test.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
 #include "straph.h"
 
 /**
@@ -20,88 +21,36 @@ void _fail(const char* msg, int line, const char* func){
 }
 #define fail(x) _fail(x, __LINE__, __func__)
 
-#include <unistd.h>
-int asd;
-void* test(node n){
-    size_t l = 0;
-    size_t w = 0;
-    char buf[10] = {0x0};
-    w = st_write(n,0,"hello",6);
-    l = st_read(n,0,buf,6);
-    printf("%d--> write: %ld read: %ld \"%s\"\n",asd++,w,l,buf); 
+void* nodewrite(node n){
+    char buf = 'A';
+    st_write(n,0,&buf,1);
     return NULL;
 }
 
-#define AM 10
-void* test2(node n){
-    long int s = 0;
-    puts("node2");
-
-    while (s < AM){
-        s++;
-        puts("--node2 write--");
-        st_write(n,0,&s,sizeof s);
-/*        puts("--node2 read--");
-        st_read(n,0,&s,sizeof s); 
-        printf("%ld <--\n",s); */
-    }
-    return NULL;
+void* readandprint(node n){
+    char buf;
+    st_read(n,0,&buf,1);
+    printf("%c %x\n",buf,buf);
+    if (buf == 'A') return NULL;
+    else return (void*) 0xff;
 }
 
-void* test3(node n){
-    long int s = 0;
-    puts("node3");
-
-    while (s < AM){
-        puts("--node3 read--");
-        st_read(n,0,&s,sizeof s);
-        printf("    --> %ld\n",s);
-      /*  s++;
-        puts("--node3 write--");
-        st_write(n,0,&s,sizeof s); */
-    }
-    return NULL;
-}
-
-
-
-#define NN 10
 int main(void){
     straph s = st_create();
-/*
-    node ns[NN];
-   
-    int i;
-    asd = 0;
-    for (i =0; i<NN; i++){
-        ns[i] = st_makenode(test);
-        if (ns[i] == NULL) fail("makenode");
-    } 
+    node n1 = st_makenode(nodewrite);
+    node n2 = st_makenode(readandprint);
+    int ret = 0;
 
-    for (i=0; i< NN-1; i++){
-        if (st_setbuffer(ns[i], 0, CIR_BUF, 100) != 0) fail("st_setbuffer");
-        if (st_nlink(ns[i],ns[i+1], PAR_MODE) != 0) fail("link_nodes");
-        if (st_addflow(ns[i],0,ns[i+1],0) != 0) fail("st_addflow");
-    }
-    
-    if (st_addnode(s, ns[0]) != 0) fail("addnode");
-    if (st_start(s) != 0) fail("st_start");
-    if (st_join(s) != 0) fail("st_join");
-    if (st_destroy(s) != 0) fail("straph_destroy");
-*/
-
-    node n2 = st_makenode(test2);
-    node n3 = st_makenode(test3);
-    st_setbuffer(n2, 0, CIR_BUF, 120);
-    st_setbuffer(n3, 0, CIR_BUF, 120);
-    st_addflow(n2,0,n3,0);
-    st_addflow(n3,0,n2,0);
-    st_addnode(s, n2);
-    st_addnode(s, n3); 
+    st_addnode(s, n1);
+    st_nlink(n1,n2,SEQ_MODE);
+    st_addflow(n1,0,n2,0);
     st_start(s);
     st_join(s);
-    
-    return 0;
+
+    if (n1->ret != 0 || n2->ret != 0) 
+        ret = EXIT_FAILURE;
+    st_destroy(s);
+    return ret;
 }
 
 
